f196b3fdb277663db828fb7670465e4c372c12cc
Joshua F. Wiley <josh@elkhartgroup.com>
2015-07-24 18:21:42 +0000

    version 0.2.1

---
+++ R/varian.R
@@ -0,0 +1,10 @@
+#' @title The Variability Analysis in R Package
+#' @docType package
+#' @name varian
+#' @import rstan
+#' @import parallel
+#' @import grid
+#' @import ggplot2
+#' @import gridExtra
+#' @import stats
+NULL
